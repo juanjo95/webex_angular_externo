1b21488008b26c3c2b40b073da862ce71de057ea
Juan J Salgado <zallagox95@hotmail.com>
2023-07-14 09:41:36 -0500

    Feat 🧨: Implementacion webex externo v2 - postman json

--- src/app/webex/webex.component.ts
+++ src/app/webex/webex.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -6,14 +6,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './webex.component.html',
   styleUrls: ['./webex.component.css']
 })
-export class WebexComponent implements AfterViewInit {
+export class WebexComponent {
 
   resp:any;
+  access_token!:string;
+  refresh_token!:string;
 
   constructor(private http: HttpClient) {}
 
-  ngAfterViewInit(): void {
-    const accessToken = 'YWI1ZDM0ZWQtMTQ4OC00YjhhLWIwYzQtYzUxOWVmZDQ2YTEwNzA1OTc3N2QtMzYz_P0A1_ea5a2209-b8f9-43f6-94fd-33c82d62e74c';
+  createMeeting(){
+    const accessToken = 'MmIyNTM0MWQtMmRjYS00NGE2LWI5MTEtZTgwNGZiZTc1NjYwMGY3NjQ0MTgtZjZi_P0A1_ea5a2209-b8f9-43f6-94fd-33c82d62e74c';
 
     const headers = new HttpHeaders({
       Authorization: `Bearer ${accessToken}`,
@@ -22,23 +24,25 @@ export class WebexComponent implements AfterViewInit {
 
     const body = {
       title: 'Mi reunión de ejemplo',
-      start: '2023-07-13T10:00:00Z', // Fecha y hora de inicio de la reunión en formato ISO 8601
-      end: '2023-07-13T11:00:00Z' // Fecha y hora de finalización de la reunión en formato ISO 8601
+      start: '2023-08-15T10:00:00Z', // Fecha y hora de inicio de la reunión en formato ISO 8601
+      end: '2023-08-15T11:00:00Z' // Fecha y hora de finalización de la reunión en formato ISO 8601
     };
 
     this.http
       .post('https://webexapis.com/v1/meetings', body, { headers })
       .subscribe((response) => {
-        console.log('Reunión creada:', response);
+        console.log('Reunión creada exitosamente:', response);
         this.resp = response;
         console.log(this.resp);
-        //this.redirectToMeeting();
-      });
-  }
 
-  redirectToMeeting() {
+        //this.redirectToMeeting();
+
+      });
+    }
+
+    redirectToMeeting() {
     const meetingUrl = this.resp.webLink;
-    window.open(meetingUrl, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=100,width=800,height=600');
+    window.open(meetingUrl, '_blank');
   }
 
 
